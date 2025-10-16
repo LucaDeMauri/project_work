f4a505feee51c05d42fa0b880ecb95ee117a7434
LucaDeMauri <luca.demauri.2006@gmail.com>
2025-10-16 12:42:27 +0200

    inizializzazione

---
+++ itsprojectwork/itsprojectwork/src/main/java/dev2426/itsprojectwork/ItsprojectworkApplication.java
@@ -0,0 +1,13 @@
+package dev2426.itsprojectwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsprojectworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItsprojectworkApplication.class, args);
+	}
+
+}
---
+++ itsprojectwork/itsprojectwork/src/test/java/dev2426/itsprojectwork/ItsprojectworkApplicationTests.java
@@ -0,0 +1,13 @@
+package dev2426.itsprojectwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsprojectworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
