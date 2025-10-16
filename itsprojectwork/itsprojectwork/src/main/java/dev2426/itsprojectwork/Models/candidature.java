181db1659d9050cf9af13397be354203e555577d
rJulius2426 <rromakin@studenti.laziodigital.it>
2025-10-16 16:02:37 +0200

    Definizione Models
    Modelli annunci, aziende, candidature

--- itsprojectwork/itsprojectwork/src/main/java/dev2426/itsprojectwork/Models/candidature.java
+++ itsprojectwork/itsprojectwork/src/main/java/dev2426/itsprojectwork/Models/candidature.java
@@ -1,5 +1,20 @@
 package dev2426.itsprojectwork.Models;
 
-public class candidature {
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
+@Table(name="candidature")
+public class candidature {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String stato;
+	private long idUtenti;
+	private long idAnnunci;
+	
 }
