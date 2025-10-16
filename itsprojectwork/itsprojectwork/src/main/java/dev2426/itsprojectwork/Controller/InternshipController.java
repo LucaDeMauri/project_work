892359ce12405ca71aab59830b285e9d049d8637
rJulius2426 <rromakin@studenti.laziodigital.it>
2025-10-16 14:33:36 +0200

    test ciao

--- itsprojectwork/itsprojectwork/src/main/java/dev2426/itsprojectwork/Controller/InternshipController.java
+++ itsprojectwork/itsprojectwork/src/main/java/dev2426/itsprojectwork/Controller/InternshipController.java
@@ -1,5 +1,11 @@
 package dev2426.itsprojectwork.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class InternshipController {
 
+	@GetMapping("")
+	public String ciao() {
+		return "ciao";
+	}
 }
