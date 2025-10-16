6eeac72ad425fa6194df2b2c2e15657f9badf95c
Fortu05 <airhunfortunate@gmail.com>
2025-10-16 16:36:46 +0200

    modifica classi model

--- itsprojectwork/itsprojectwork/src/main/java/dev2426/itsprojectwork/Models/competenze.java
+++ itsprojectwork/itsprojectwork/src/main/java/dev2426/itsprojectwork/Models/competenze.java
@@ -1,5 +1,34 @@
 package dev2426.itsprojectwork.Models;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table(name="competenze")
 public class competenze {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String descrizione;
+
+    public competenze(long id,String descrizione){
+        this.id=id;
+        this.descrizione=descrizione;
+    }
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
 }
--- itsprojectwork/itsprojectwork/src/main/java/dev2426/itsprojectwork/Models/ruoli.java
+++ itsprojectwork/itsprojectwork/src/main/java/dev2426/itsprojectwork/Models/ruoli.java
@@ -1,5 +1,44 @@
 package dev2426.itsprojectwork.Models;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table(name="ruoli")
 public class ruoli {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String tipo;
+    private long id_utente;
+
+    public ruoli(long id,String tipo,long id_utente){
+        this.id=id;
+        this.tipo=tipo;
+        this.id_utente=id_utente;
+    }
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public long getId_utente() {
+        return id_utente;
+    }
+    public void setId_utente(long id_utente) {
+        this.id_utente = id_utente;
+    }
 
 }
