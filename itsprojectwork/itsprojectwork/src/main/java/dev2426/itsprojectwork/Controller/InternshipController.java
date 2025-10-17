0ffc4028415387eb96c732f92ed562c3203af5ac
rJulius2426 <rromakin@studenti.laziodigital.it>
2025-10-17 11:09:10 +0200

    Dashboard aggiornato 1

--- itsprojectwork/itsprojectwork/src/main/java/dev2426/itsprojectwork/Controller/InternshipController.java
+++ itsprojectwork/itsprojectwork/src/main/java/dev2426/itsprojectwork/Controller/InternshipController.java
@@ -1,5 +1,25 @@
 package dev2426.itsprojectwork.Controller;
 
-public class InternshipController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import dev2426.itsprojectwork.Services.AnnunciService;
+
+@Controller
+@RequestMapping("/internship/")
+public class InternshipController {
+	
+	@Autowired
+	private AnnunciService servizioAnnunci;
+	
+	@GetMapping("dashboard")
+	public String dashboard(Model modelloDB) {
+		
+		modelloDB.addAttribute("listaAnnunci", servizioAnnunci.getAll());
+		
+		return "Internship/Dashboard";
+	}
 }
--- itsprojectwork/itsprojectwork/src/main/java/dev2426/itsprojectwork/Models/Annuncio.java
+++ itsprojectwork/itsprojectwork/src/main/java/dev2426/itsprojectwork/Models/Annuncio.java
@@ -1,9 +1,13 @@
 package dev2426.itsprojectwork.Models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -16,9 +20,13 @@ public class Annuncio {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
+	@Column(name="tipo_mansione")
 	private String tipoMansione;
-	private int durata;
-	private long idAzienda;
 	
+	private int durata;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "idAzienda")
+	private Azienda azienda;
 
 }
