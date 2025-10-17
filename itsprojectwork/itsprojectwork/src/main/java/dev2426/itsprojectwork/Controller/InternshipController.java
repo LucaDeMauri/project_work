73a4d1a69e0e71eb1f5e9edf80bbae24c4a544e9
Fortu05 <airhunfortunate@gmail.com>
2025-10-17 11:29:39 +0200

    controlle e connection

--- itsprojectwork/itsprojectwork/src/main/java/dev2426/itsprojectwork/Controller/InternshipController.java
+++ itsprojectwork/itsprojectwork/src/main/java/dev2426/itsprojectwork/Controller/InternshipController.java
@@ -1,5 +1,16 @@
 package dev2426.itsprojectwork.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/intship/")
 public class InternshipController {
+
+    @GetMapping("dashboard")
+    public String dashPage(){
+        return "/Internship/Dashboard";
+    }
 
 }
