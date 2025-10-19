fdd564213133cc33a746debc4b0c8a6315963933
gianninoconino <lucdm95@gmail.com>
2025-10-19 19:33:22 +0200

    admin

--- itsprojectwork/src/main/java/dev2426/itsprojectwork/controller/AdminController.java
+++ itsprojectwork/src/main/java/dev2426/itsprojectwork/controller/AdminController.java
@@ -1,7 +1,32 @@
 package dev2426.itsprojectwork.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import dev2426.itsprojectwork.models.Ruolo;
+import dev2426.itsprojectwork.models.Utente;
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/admin")
 public class AdminController {
 	
+	 @GetMapping("")
+	 public String adminPage(HttpSession session) {
+		 
+		 Utente utenteLoggato = (Utente) session.getAttribute("utenteLoggato");
+		 
+		 System.out.println(utenteLoggato.getRuolo());
+		 System.out.println(utenteLoggato.getRuolo().equals(Ruolo.admin));
+		 
+		 if(utenteLoggato.getRuolo().equals(Ruolo.admin)){
+			 return "/internship/admin";
+		 }
+		 
+		 return "/internship/";
+		 
+	 }
 	
 
 }
